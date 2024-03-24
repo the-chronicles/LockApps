a323420670f4c5fdbdf5f289d69fb9219ccf656e
the-chronicles <oluseungideon@gmail.com>
2024-03-24 23:53:26 +0100

    social media

---
+++ utils/socialMediaApps.js
@@ -0,0 +1,22 @@
+// utils/socialMediaApps.js
+
+// Sample list of social media apps
+export const SocialMediaApps = [
+  {
+    id: 1,
+    name: "Facebook",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+  },
+  {
+    id: 3,
+    name: "Twitter",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+  },
+  // Add more social media apps as needed
+];
